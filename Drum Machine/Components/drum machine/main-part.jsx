147714a2ac5f1aecfbb90bece6ad53a35246cb03
hazybane <patukroman@gmail.com>
2023-03-15 09:05:54 +0100

    Drum machine ready.

--- Drum Machine/Components/drum machine/main-part.jsx
+++ Drum Machine/Components/drum machine/main-part.jsx
@@ -2,7 +2,8 @@ import React, { useState} from 'react';
 import {Soundsnames} from "./sounds.jsx";
 import {DrumPad} from "./drum-pad.jsx";
 import '/Drum Machine/Styles/main-part.scss'
-import{audioSamples} from "../../../public/Sounds/samples.jsx";
+
+import {audioSamples} from "../../samples.jsx";
 
 const MainPart = () => {
     const [resetKey, setResetKey] = useState(0);
---
+++ Drum Machine/samples.jsx
@@ -0,0 +1,8 @@
+const audioSamples = [
+    { url: "/Drum Machine/Assets/Sounds/kick.wav"},
+    { url: "/Drum Machine/Assets/Sounds/snare.wav"},
+    { url: "/Drum Machine/Assets/Sounds/open-hat.wav"},
+    { url: "/Drum Machine/Assets/Sounds/hi-hat.wav"},
+]
+
+export {audioSamples}
